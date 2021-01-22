bf178e1b60695e193fcaefa98e4ec14a2510c797
Matthew Mattox <Matt.Mattox@suse.com>
2021-01-22 01:32:23 -0600

    Updating DB scheme

--- data.sql
+++ data.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE `Data` ;
-CREATE TABLE `Data`.`Connections` (
-  `id` INT NOT NULL,
-  `timestamp` DATETIME NULL DEFAULT NOW(),
-  `nodename` VARCHAR(45) NULL,
-  `podname` VARCHAR(45) NULL,
-  `podip` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `Data` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `Data`;
+DROP TABLE IF EXISTS `Connections`;
+CREATE TABLE `Connections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  `nodename` varchar(45) DEFAULT NULL,
+  `podname` varchar(45) DEFAULT NULL,
+  `podip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
